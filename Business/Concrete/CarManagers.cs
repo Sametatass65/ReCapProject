f2932ea4d00f21cc42493f02fc63bd67f46df8fe
Samet <sametatass@gmail.com>
2022-11-01 17:12:47 +0300

    Add project files.

---
+++ Business/Concrete/CarManagers.cs
@@ -0,0 +1,39 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManagers : ICarService
+    {
+        ICarDal _carDal;
+        public CarManagers(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+        public void Add(Car car)
+        {
+            _carDal.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+CarManagers cars = new CarManagers(new InMemoryCarDal());
+
+foreach (var car in cars.GetAll())
+{
+    Console.WriteLine(car.Description);
+}
